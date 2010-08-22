dcb94be34e14cf48c7e40eba107df494de3c9c71
Till Klampaeckel <till@php.net>
2010-08-22 19:09:17 +0200

    chef dir

--- solo.rb
+++ solo.rb
@@ -1,6 +1,7 @@
 this_dir = File.dirname(__FILE__)
+chef_dir = "#{this_dir}/chef-solo"
 
-file_cache_path "#{this_dir}/chef-solo"
-cookbook_path [ "#{this_dir}/chef-solo/cookbooks" ]
-role_path "#{this_dir}/chef-solo/roles"
+file_cache_path "#{chef_dir}"
+cookbook_path [ "#{chef_dir}/cookbooks" ]
+role_path "#{chef_dir}/roles"
 recipe_url "http://github.com/till/easybib-cookbooks/zipball/master"
