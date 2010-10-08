3e86837962dc0063cf2e79dee5666175f5cc98eb
till <till@php.net>
2010-10-08 18:27:10 +0200

    * download tgz instead [zip don't work]  * create chef-solo dir - just to make sure

--- solo.rb
+++ solo.rb
@@ -1,7 +1,11 @@
 this_dir = File.dirname(__FILE__)
 chef_dir = "#{this_dir}/chef-solo"
 
+if !File.exist?(chef_dir) then
+    Dir.mkdir(chef_dir)
+end
+
 file_cache_path "#{chef_dir}"
 cookbook_path [ "#{chef_dir}/cookbooks" ]
 role_path "#{chef_dir}/roles"
-recipe_url "http://github.com/till/easybib-cookbooks/zipball/master"
+recipe_url "http://github.com/till/easybib-cookbooks/tarball/master"
