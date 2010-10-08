f7f5b6730cbb2fd44f7a1b5e7e5a451741f8c027
till <till@php.net>
2010-10-08 18:29:25 +0200

    * install Ruby enterprise instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,28 @@
 #                                                                                  #
 ####################################################################################
 
-apt-get install -y ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert
+apt-get install -y build-essential wget ssl-cert
+
+RUBY_DEB=ruby-enterprise_1.8.7-2010.02_amd64_ubuntu8.04.deb
+wget http://rubyforge.org/frs/download.php/71097/$RUBY_DEB --quiet -O ./$RUBY_DEB
+
+if [ -f ./$RUBY_DEB ]; then
+    dpkg -i $RUBY_DEB
+
+    if [ "$?" == "0" ]; then
+
+        gem install -y ohai
+        gem install -y chef
+
+    else
+        echo "Could not install Ruby Enterprise."
+        exit 1
+    fi
+
+else
+    echo "Could not download Ruby Enterprise."
+    exit 1
+fi
+
 gem install -y ohai
 gem install -y chef
